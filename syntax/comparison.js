db1fb58f6a2f6f7c00e8f4b4545f63af33f09aa3
YSY986 <129840175+YSY986@users.noreply.github.com>
2023-04-13 15:40:56 +0900

    230411

---
+++ syntax/comparison.js
@@ -0,0 +1,5 @@
+console.log(1===1); // true
+console.log(1==2); // false
+console.log(1>2); 
+console.log(1<2);
+name = 1; // 대입 연산자
---
+++ syntax/function2.js
@@ -0,0 +1,12 @@
+console.log(Math.round(1.6));   //2
+console.log(Math.round(1.4));   //1
+
+function sum(a, b) {    // parameter
+    console.log('a');
+    return a+b;
+    console.log('b');
+};
+
+console.log(sum(2,4));   // argument, 인자
+
+Math.round(1.4);
